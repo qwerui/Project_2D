0aee83bc65a4eebb9918aca9acaeebbe596eb7fe
fenoteryn <39932013+qwerui@users.noreply.github.com>
2022-07-15 17:19:30 +0900

    ver 0.19.2
    Next To Do

---
+++ Assets/NextDevelop.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextDevelop : MonoBehaviour
+{
+    /*
+     * Add GameOverScene BGM
+     * Add GameOverScene Counting SFX
+     */
+}
